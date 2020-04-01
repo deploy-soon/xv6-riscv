67e6e4c1d823eea34366749f63ac3129baf12f5b
soon <dyd3312@gmail.com>
2020-04-01 18:08:09 +0900

    before project

--- user/ulib.c
+++ user/ulib.c
@@ -88,11 +88,12 @@ int
 atoi(const char *s)
 {
   int n;
+  int sign = (*s == '-')? s++, -1: 1;
 
   n = 0;
   while('0' <= *s && *s <= '9')
     n = n*10 + *s++ - '0';
-  return n;
+  return sign * n;
 }
 
 void*
